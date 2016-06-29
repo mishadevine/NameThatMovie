b825443b3c6faad58be61d84e91222fdeaad992c
mishadevine <mdevine@Mishas-MacBook-Pro.local>
2016-06-28 21:13:28 -0400

    converted components into react-native files bye importing react-native in each component

---
+++ app/components/Answers.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  TextInput,
+  ListView,
+  Image,
+  Navigator
+} from 'react-native';
+
+class Answers extends Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.wrapper}>
+        </View>
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1976d2',
+  },
+});
+
+
+
+module.exports = Answers;
