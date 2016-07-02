f2212f6cc0a0735c04337a3d4148dcab8bb64649
Misha Devine <mdevine@fullsail.edu>
2016-07-02 15:18:45 -0400

    created the controller for the recommended ovies page

---
+++ public/assets/js/recMoviesCtrl.js
@@ -0,0 +1,31 @@
+angular.module("NameThatMovie")
+  .controller("RecMoviesCtrl", function($scope,$firebaseArray,$firebaseAuth){
+
+    //Connect to Firebase
+    var ref = new Firebase("https://namethatmovie3.firebaseio.com/");
+
+    $scope.authObj = $firebaseAuth(ref);
+    var movies;
+
+    $scope.authObj.$onAuth(function(authData) {
+    if (authData) {
+      var URL = "https://namethatmovie3.firebaseio.com/users/" + authData.uid + "/recommendations";
+      $scope.userID = authData.uid;
+      var listRef = new Firebase(URL);
+      movies = $firebaseArray(listRef);
+      $scope.movies = movies;
+      } else {
+      console.log("Logged out");
+      }
+    });
+
+    $scope.movies = movies;
+
+    // Remove from database
+    $scope.removeMovie = function(movie) {
+      $scope.movies.$remove(movie).then(function(ref) {
+        console.log("removed movie");
+      });
+    }
+
+  });
