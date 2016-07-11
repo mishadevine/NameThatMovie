9c37b87dd632c79295a92ab031c61d86d6f68dc5
Misha Devine <mdevine@fullsail.edu>
2016-07-11 16:32:24 -0400

    displaying questions from the database

--- public/assets/js/gameCtrl.js
+++ public/assets/js/gameCtrl.js
@@ -6,10 +6,15 @@ angular.module("NameThatMovie")
     $firebaseObject(ref)
     var questions;
 
-    var URL = "https://namethatmovie3.firebaseio.com/questionsANDanswers/Questions"
+    var URL = "https://namethatmovie3.firebaseio.com/questionsANDanswers"
     var listRef = new Firebase(URL);
     questions = $firebaseArray(listRef);
     $scope.questions = questions;
+
+    // var URL = "https://namethatmovie3.firebaseio.com/questionsANDanswers/Answers"
+    // var listRef = new Firebase(URL);
+    // answers = $firebaseArray(listRef);
+    // $scope.questions = answers;
 
 
     listRef.limitToFirst(1).on("value", function(snapshot) {
